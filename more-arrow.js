cf04e56b70b62ccf0bb139bb7f5f573db055eea2
adil4260 <adilsarkar001@gmail.com>
2026-09-06 02:20:35 +0800

    full module1

--- more-arrow.js
+++ more-arrow.js
@@ -7,4 +7,4 @@ const logIt = () => console.log(78)
 console.log(getSquare(5)); // output: 25
 console.log(getHalf(25))
 console.log(firstElement([1, 2, 3, 4, 5])); // output: 2
-console.log(logIt()); // output: 78
+console.log(logIt()); // output:undefined
